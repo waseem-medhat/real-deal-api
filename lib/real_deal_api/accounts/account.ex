fd74e94e04e1cd52cf1c2a4b297430cecd99d494
wipdev-tech <wipdev.tech@gmail.com>
2024-05-11 14:08:26 +0300

    Generate accounts table and related MVC

---
+++ lib/real_deal_api/accounts/account.ex
@@ -0,0 +1,20 @@
+defmodule RealDealApi.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :email, :string
+    field :hash_password, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hash_password])
+    |> validate_required([:email, :hash_password])
+  end
+end
